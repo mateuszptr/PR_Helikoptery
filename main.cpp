bd4be36492275324a7b62e5cdfc24a9cd7cadb7a
eobard <eobard@localhost.localdomain>
2017-05-30 20:45:00 +0200

    add mutexes

--- main.cpp
+++ main.cpp
@@ -4,8 +4,22 @@ timestamp ts, ts_r;
 State state_hangar;
 State state_start;
 
-int RD_H[H]={0};
-int RD_S[H]={0};
+pthread_mutex_t mutex_state_hangar;
+pthread_mutex_t mutex_state_start;
+pthread_mutex_t mutex_ts;
+pthread_mutex_t mutex_ts_r;
+pthread_mutex_t mutex_rd_h;
+pthread_mutex_t mutex_rd_s;
+
+pthread_mutexattr_t mutex_state_hangar_attr;
+pthread_mutexattr_t mutex_state_start_attr;
+pthread_mutexattr_t mutex_ts_attr;
+pthread_mutexattr_t mutex_ts_r_attr;
+pthread_mutexattr_t mutex_rd_h_attr;
+pthread_mutexattr_t mutex_rd_s_attr;
+
+int RD_H[H] = {0};
+int RD_S[H] = {0};
 
 int rank, size;
 char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -53,50 +67,117 @@ void init(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Get_processor_name(processor_name, &namelen);
-    
+
     ts = timestamp(0, rank);
     state_hangar = UNINTERESTED;
     state_start = UNINTERESTED;
-    
+
     finished = false;
+
+    init_mutex();
+
+    pthread_t monitor_handle;
+    pthread_create(&monitor_handle, NULL, monitor, NULL);
+
+
+
+}
+
+void init_mutex() {
+    pthread_mutexattr_init(&mutex_state_hangar_attr);
+    pthread_mutex_init(&mutex_state_hangar, &mutex_state_hangar_attr);
+
+
+    pthread_mutexattr_init(&mutex_state_start_attr);
+    pthread_mutex_init(&mutex_state_start, &mutex_state_start_attr);
+
+
+    pthread_mutexattr_init(&mutex_ts_attr);
+    pthread_mutex_init(&mutex_ts, &mutex_ts_attr);
+
+    pthread_mutexattr_init(&mutex_ts_r_attr);
+    pthread_mutex_init(&mutex_ts_r, &mutex_ts_r_attr);
     
+    pthread_mutexattr_init(&mutex_rd_h_attr);
+    pthread_mutex_init(&mutex_rd_h, &mutex_rd_h_attr);
+    
+    pthread_mutexattr_init(&mutex_rd_s_attr);
+    pthread_mutex_init(&mutex_rd_s, &mutex_rd_s_attr);
+
 }
 
 void finalize() {
     printf("h%d: Kończę!\n", rank);
     MPI_Finalize();
+
+    pthread_mutex_destroy(&mutex_state_hangar);
+    pthread_mutex_destroy(&mutex_state_start);
+    pthread_mutex_destroy(&mutex_ts);
+    pthread_mutex_destroy(&mutex_ts_r);
+    pthread_mutex_destroy(&mutex_rd_h);
+    pthread_mutex_destroy(&mutex_rd_s);
 }
 
 void req_hangar() {
-    ts_r = ts;
+
+    
+    pthread_mutex_lock(&mutex_ts);
+    pthread_mutex_lock(&mutex_ts_r);
+    
     sendAll(REQ_HANGAR, ts_r);
+    ts_r = ts;
+    
+    pthread_mutex_unlock(&mutex_ts_r);
+    pthread_mutex_unlock(&mutex_ts);
     //recv_h
     //wait
 }
 
 void req_start() {
-    ts_r = ts;
+    
+    pthread_mutex_lock(&mutex_ts);
+    pthread_mutex_lock(&mutex_ts_r);
+    
     sendAll(REQ_START, ts_r);
+    ts_r = ts;
+    
+    pthread_mutex_unlock(&mutex_ts_r);
+    pthread_mutex_unlock(&mutex_ts);
+
     //recv_s
     //wait
 }
 
 void rel_hangar() {
+    pthread_mutex_lock(&mutex_state_hangar);
     state_hangar = UNINTERESTED;
-    for(int i=0;i<H;i++) {
-        if(RD_H[i]==1) {
+    pthread_mutex_unlock(&mutex_state_hangar);
+    
+    pthread_mutex_lock(&mutex_ts);
+    pthread_mutex_lock(&mutex_rd_h);
+    for (int i = 0; i < H; i++) {
+        if (RD_H[i] == 1) {
             send(i, REL_HANGAR, ts);
-            RD_H[i]=0;
+            RD_H[i] = 0;
         }
     }
+    pthread_mutex_unlock(&mutex_rd_h);
+    pthread_mutex_unlock(&mutex_ts);
 }
 
 void rel_start() {
+    pthread_mutex_lock(&mutex_state_start);
     state_start = UNINTERESTED;
-    for(int i=0;i<H;i++) {
-        if(RD_S[i]==1) {
+    pthread_mutex_unlock(&mutex_state_start);
+    
+    pthread_mutex_lock(&mutex_ts);
+    pthread_mutex_lock(&mutex_rd_s);
+    for (int i = 0; i < H; i++) {
+        if (RD_S[i] == 1) {
             send(i, REL_START, ts);
-            RD_S[i]=0;
+            RD_S[i] = 0;
         }
     }
+    pthread_mutex_unlock(&mutex_rd_s);
+    pthread_mutex_unlock(&mutex_ts);
 }
--- monitor.cpp
+++ monitor.cpp
@@ -4,15 +4,22 @@ extern timestamp ts, ts_r;
 extern State state_hangar;
 extern State state_start;
 
+extern pthread_mutex_t mutex_state_hangar;
+extern pthread_mutex_t mutex_state_start;
+extern pthread_mutex_t mutex_ts;
+extern pthread_mutex_t mutex_ts_r;
+extern pthread_mutex_t mutex_rd_h;
+extern pthread_mutex_t mutex_rd_s;
+
 void* monitor(void* arg) {
-    
+
     timestamp ts_r;
     Message msg;
     int p;
-    
-    while(1) {
+
+    while (1) {
         recv(&p, &msg, &ts_r);
-        switch(msg) {
+        switch (msg) {
             case REQ_HANGAR:
                 on_req_hangar(p, msg, ts_r);
                 break;
@@ -30,29 +37,53 @@ void* monitor(void* arg) {
 }
 
 void on_req_hangar(int receiver, Message msg, timestamp ts_i) {
-    if(state_hangar != BUSY && (state_hangar == UNINTERESTED || ts_i < ts_r)) {
+    pthread_mutex_lock(&mutex_state_hangar);
+    pthread_mutex_lock(&mutex_ts);
+    pthread_mutex_lock(&mutex_ts_r);
+    pthread_mutex_lock(&mutex_rd_h);
+
+    if (state_hangar != BUSY && (state_hangar == UNINTERESTED || ts_i < ts_r)) {
         send(receiver, REL_HANGAR, ts);
     } else {
         RD_H[receiver] = 1;
     }
+
+    pthread_mutex_unlock(&mutex_rd_h);
+    pthread_mutex_unlock(&mutex_ts_r);
+    pthread_mutex_unlock(&mutex_ts);
+    pthread_mutex_unlock(&mutex_state_hangar);
 }
 
 void on_req_start(int receiver, Message msg, timestamp ts_i) {
-    if(state_start != BUSY && (state_start == UNINTERESTED || ts_i < ts_r)) {
+    pthread_mutex_lock(&mutex_state_start);
+    pthread_mutex_lock(&mutex_ts);
+    pthread_mutex_lock(&mutex_ts_r);
+    pthread_mutex_lock(&mutex_rd_s);
+
+    if (state_start != BUSY && (state_start == UNINTERESTED || ts_i < ts_r)) {
         send(receiver, REL_START, ts);
     } else {
         RD_S[receiver] = 1;
     }
+
+    pthread_mutex_unlock(&mutex_rd_s);
+    pthread_mutex_unlock(&mutex_ts_r);
+    pthread_mutex_unlock(&mutex_ts);
+    pthread_mutex_unlock(&mutex_state_start);
 }
 
 void on_rel_hangar(int receiver, Message msg, timestamp ts_i) {
-    if(ts_r < ts_i) {
+    pthread_mutex_lock(&mutex_ts_r);
+    if (ts_r < ts_i) {
         //recv_h++
     }
+    pthread_mutex_unlock(&mutex_ts_r);
 }
 
 void on_rel_start(int receiver, Message msg, timestamp ts_i) {
-    if(ts_r < ts_i) {
+    pthread_mutex_lock(&mutex_ts_r);
+    if (ts_r < ts_i) {
         //recv_s++
     }
+    pthread_mutex_unlock(&mutex_ts_r);
 }
